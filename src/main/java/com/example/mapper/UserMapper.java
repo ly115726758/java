ea0b51e275e5ed81268df5bf88ad1b4dd7cc7172
ly <1145726758@qq.com>
2018-01-10 11:10:45 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/example/controller/DemoApplication.java

--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -10,10 +10,15 @@ import java.util.Map;
  * Created by Administrator on 2018/1/2.
  */
 @Component
-
 public interface UserMapper {
 
-
+     /**
+     * @Author : 凌勇.
+     * @Description :
+     * @Params  :
+     * @ReturnType :
+     * @Date : 11:04, 2018/1/10
+     */
      User getUser(Map<Object,Object> map);
 
 }
